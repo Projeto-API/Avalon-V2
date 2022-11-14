9ffeeb04c0ba8e1c217b2aebeea5ff32ae295bec
paloma <palomavillasboas380@gmail.com>
2022-11-14 16:00:42 -0300

    js errors validacoes

--- js/validacoescadastroproduto.js
+++ js/validacoescadastroproduto.js
@@ -3,24 +3,39 @@ window.addEventListener('load', function(){
     
     formularioForm.addEventListener('submit', function(event){
     event.preventDefault();
+    let errorsMessages = [];
     
     let formtitulo = document.getElementById('titulo');
     if(formtitulo.value == ''){
-    alert('O campo não pode estar vazio')
+    errorsMessages.push('O campo não pode estar vazio')
     }else if (formtitulo.value.length < 3){
-        alert('O tamanho precisa ser maior que 3')
+        errorsMessages.push('O tamanho precisa ser maior que 3')
     };
     let formpublic = document.getElementById('public');
     if(formpublic.value == ''){
-        alert('O campo não pode estar vazio')
+        errors.push('O campo não pode estar vazio')
         }else if (formpublicac.value.length < 3){
-            alert('O tamanho precisa ser maior que 3')
+            errorsMessages.push('O tamanho precisa ser maior que 3')
         };
         let formautordolivro = document.getElementById('autordolivro');
     if(formautordolivro.value == ''){
-        alert('O campo não pode estar vazio')
+
+        errorsMessages.push('O campo não pode estar vazio')
         }else if (formautordolivro.value.length < 3){
-            alert('O tamanho precisa ser maior que 3')
+            errorsMessages.push('O tamanho precisa ser maior que 3')
         };
+        let Messages = document.getElementById('message');
+        if(Messages.value == ''){
+            errorsMessages.push('O campo não pode estar vazio')
+        }
+        if(errorsMessages.leght > 0){
+        event.preventDefault();
+        }
+        let ulErrors = document.querySelector('div.errors ul');
+        for(let i = 0; i < errorsMessages.leght; i++){
+ulErrors.innerHTML += '<li>' + '</li>'
+        }
     })
     })
+
+

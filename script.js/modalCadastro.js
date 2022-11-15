5499d52e3d054a4884de11a3bc7b44d06c62b8d0
Joao <pouljp@bol.com.br>
2022-11-15 01:19:13 -0300

    Site Finalizado, modal ok

--- script.js/modalCadastro.js
+++ script.js/modalCadastro.js
@@ -3,5 +3,5 @@ function abrirModal() {
     document.getElementById('bg-modal').style.top ="0";
 }
 function fecharModal() {
-    document.getElementById('bg-modal').style.top = "-100%";
+    document.getElementById('bg-modal').style.top = "-150%";
 }

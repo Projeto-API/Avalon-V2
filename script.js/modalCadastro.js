81ea88d15ac749d51bbe5ba7f27f77dd2c045463
Joao <pouljp@bol.com.br>
2022-11-15 13:16:28 -0300

    Ajustando as regras de Formulario junto ao Pi

--- script.js/modalCadastro.js
+++ script.js/modalCadastro.js
@@ -5,3 +5,40 @@ function abrirModal() {
 function fecharModal() {
     document.getElementById('bg-modal').style.top = "-150%";
 }
+
+
+
+function verificarFormulario(){
+ 
+    let formularioForm = document.querySelector('form.formulario')
+    formularioForm.addEventListener('submit', function(enviarDados){
+
+        let tituloLivro = document.getElementById("tituloLivro");
+      
+        if(tituloLivro.value = " "){
+            alert("O campo Livro não pode estar vazio")
+            
+        } else{
+            alert("ok salvo")
+            enviarDados.preventDefault();
+        }
+        let autor = document.getElementById("autor");
+      
+        if(autor.value = " "){
+            alert("O campo Autor não pode estar vazio")
+            
+        } else{
+            alert("ok salvo")
+            enviarDados.preventDefault();
+        }
+
+}
+)}
+
+
+    
+    
+// })
+
+
+// })

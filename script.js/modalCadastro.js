a18eddcd78fcf45935eb52adf4695c2da720b642
Joao <pouljp@bol.com.br>
2022-11-15 18:09:26 -0300

    Inicio configuração de validações

--- script.js/modalCadastro.js
+++ script.js/modalCadastro.js
@@ -11,34 +11,40 @@ function fecharModal() {
 function verificarFormulario(){
  
     let formularioForm = document.querySelector('form.formulario')
-    formularioForm.addEventListener('submit', function(enviarDados){
-
+    formularioForm.addEventListener('submit', function(event){
+        
+        
+        let errorsMessages = [];
+        
         let tituloLivro = document.getElementById("tituloLivro");
-      
-        if(tituloLivro.value = " "){
-            alert("O campo Livro não pode estar vazio")
+        if(tituloLivro.value == '" '){
+            errorsMessages.push("O campo Livro não pode estar vazio")
             
-        } else{
-            alert("ok salvo")
-            enviarDados.preventDefault();
+        } else if(tituloLivro.value.length < 3  ) {
+            errorsMessages.push("o tamanho do Titulo do livro tem de ser maior que 3")
         }
+        
         let autor = document.getElementById("autor");
-      
-        if(autor.value = " "){
-            alert("O campo Autor não pode estar vazio")
+        if(autor.value == ''){
+            errorsMessages.push("O campo Autor não pode estar vazio")
             
-        } else{
-            alert("ok salvo")
-            enviarDados.preventDefault();
+        } else if(autor.value.length < 3  ) {
+            errorsMessages.push("o tamanho do Autor tem de ser maior que 3")
+            
         }
+      
+        if (errorsMessages.length > 0){
+            event.preventDefault();
 
-}
-)}
+            let divErrors = document.getElementById('div-errors');
+            divErrors.classList.remove('no-errors');
+            divErrors.classList.add('errors');
+            
+            let ulErrors = document.querySelector('div.errors ul');
+            for (let i = 0; i < errorsMessages.length; i++) {
+                ulErrors.innerHTML += '<li>' + errorsMessages[i] + '</li>'
 
-
-    
-    
-// })
-
-
-// })
+            }
+        }
+    })
+ }

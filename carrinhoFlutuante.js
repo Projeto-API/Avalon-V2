86481a08640a68d9af39dfae48db49ff0d4e589d
Joao <pouljp@bol.com.br>
2022-11-19 11:54:36 -0300

    carrinho Fluante

---
+++ carrinhoFlutuante.js
@@ -0,0 +1,7 @@
+
+function abrirCarrinhoFlutuante() {
+    document.getElementById('.carrinhoFlutuante').style.width ="30%";
+}
+function fecharCarrinhoFlutuante() {
+    document.getElementById('.carrinhoFlutuante').style.width = "0";
+}

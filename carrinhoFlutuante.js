1d49efb6e8e0a56b20c1df573d342269d40413d5
Joao <pouljp@bol.com.br>
2022-11-19 13:42:22 -0300

    tentando ajustar o Carrinho

--- carrinhoFlutuante.js
+++ carrinhoFlutuante.js
@@ -1,7 +1,9 @@
 
 function abrirCarrinhoFlutuante() {
-    document.getElementById('.carrinhoFlutuante').style.width ="30%";
+    alert("abrir")
+    document.getElementsByTagName('.carrinhoFlutuante').style.width = "30%";
 }
 function fecharCarrinhoFlutuante() {
-    document.getElementById('.carrinhoFlutuante').style.width = "0";
+    alert("fechar")
+    document.getElementsByTagName('.carrinhoFlutuante').style.width= "0%";
 }

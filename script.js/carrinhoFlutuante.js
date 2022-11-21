bcd22b9bc9c2687171c8fe80ad36c54e8a5ba3e1
IgorCamposRatao <igorcamposratao@gmail.com>
2022-11-21 15:55:28 -0300

    novos headers

--- script.js/carrinhoFlutuante.js
+++ script.js/carrinhoFlutuante.js
@@ -1,6 +1,6 @@
 
 function abrirCarrinhoFlutuante() {
-    document.getElementById('carrinhoFlutuante').style.width = "22rem"
+    document.getElementById('carrinhoFlutuante').style.width = "20rem"
 }
 
 function fecharCarrinhoFlutuante() {
@@ -20,6 +20,30 @@ function excluirDentroCarrinho(excluiProduto ) {
     }
     
 }
+
+
+
+// function incluirDentroCarrinho() {
+//     let em = document.createElement('em')
+//     em.innerHTML = <div class="produto" id="produto1">
+//     <img class="imagemProduto" src="../produtos/my broken mariko/mariko-capa.jpg" alt="">
+//     <div class="informationProduto">
+//         <div class="nomeProduto">
+//             <h5 id="produtoTitulo">MY BROKEN MARIKO</h5>
+//         </div>
+//         <div class="informationProdutoValue">
+//             <div>
+//                 <a href="" class="diminuirQtd">-</a>
+//                 <input type="number" class="quantidadeValue" value="1">
+//                 <a href="" class="aumentarQtd">+</a>
+//             </div>
+//             <span id="preco"> R$ 39,90 </span>
+//             <button id="excluir"  onclick="excluirDentroCarrinho('produto4')">X</button>
+//         </div>
+//     </div>
+// </div>
+//     document.querySelector('#containerProduto').appendChild(em)
+// }
 
 
 

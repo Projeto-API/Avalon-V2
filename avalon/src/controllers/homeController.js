d34677122d4fcf46824b2253dc84defb338d3817
paloma <palomavillasboas380@gmail.com>
2022-12-02 21:52:37 -0300

    nodemon

--- avalon/src/controllers/homeController.js
+++ avalon/src/controllers/homeController.js
@@ -1,6 +1,9 @@
-const homeController = {
-    index: (req, res) =>{
-        return res.render("index");
+const homeModel = require('../models/homeModel')
+
+module.exports = {
+    listaServicos: (req, res) => {
+        const home = homeModel.index();
+        //res.render('VIEW', {Objeto})
+        return res.render('servicos', {listaServicos: servicos, title: "Lista de Serviços"})
     }
-};
-module.exports =homeController;
+}

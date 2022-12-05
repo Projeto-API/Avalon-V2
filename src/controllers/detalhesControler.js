c7f01cfb5911f2dc2ec67ad3ff224a24c3307f97
Joao <pouljp@bol.com.br>
2022-12-05 20:54:14 -0300

    retorno merge

--- src/controllers/detalhesControler.js
+++
@@ -1,8 +0,0 @@
-const detalhesModel = require('../models/detalhesModel');
-
-module.exports = {
-    index: (req, res)=> {
-        const detalhes = detalhesModel.index();
-        return res.render('detalhes', detalhes)
-    }
-}
--- src/controllers/homeController.js
+++
@@ -1,6 +0,0 @@
-const homeController = {
-    index: (req, res) =>{
-        return res.render("index");
-    }
-};
-module.exports =homeController;

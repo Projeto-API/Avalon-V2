d5ed06e04c04dd4341cb76d985a959a4da56c0f1
amandakrine <amandakrine@hotmail.com>
2022-12-10 20:18:27 -0400

    ajustando pasta

--- avalon/src/controllers/estiloProdutosController.js
+++ avalon/src/controllers/estiloProdutosController.js
@@ -1,9 +1,8 @@
-const estiloProdutosModel = require('../models/estiloProdutosModel')
+const produtosModel = require('../models/produtosModel');
 
 module.exports = {
-    listaestiloProdutos: (req, res) => {
-        const estiloProdutos = estiloProdutosModel.index();
-       
-        res.render('estiloProdutos', { title: 'Express', css: 'estiloProdutos', estiloProdutos });
+    index: (req, res)=> {
+        const estiloProdutos = produtosModel.index();
+        return res.render('estiloProdutos', {estiloProdutos, javascripts: "/javascripts/adicionarAoCarrinho.js"})
     }
 }

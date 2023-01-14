14421db7812732630bfa7845f3c99bfc41b8d4d9
amandakrine <amandakrine@hotmail.com>
2023-01-14 15:34:13 -0400

    instalação multer

--- avalon/src/routes/index.js
+++ avalon/src/routes/index.js
@@ -1,5 +1,6 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const multer = require("multer");
 
 /* GET home page. */
 router.get('/', function (req, res, next) {

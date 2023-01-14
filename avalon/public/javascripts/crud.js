e7bcc6930b63084b58e06b3cb46567fa1decbec5
IgorCamposRatao <igorcamposratao@gmail.com>
2023-01-14 16:10:09 -0300

    modal crud corrigido

--- avalon/public/javascripts/crud.js
+++ avalon/public/javascripts/crud.js
@@ -5,7 +5,7 @@ var modal = document.getElementById("box-cadastro");
 var btn = document.getElementById("myBtn");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var btnCancelar = document.getElementById("cancelarModal");
 
 // When the user clicks on the button, open the modal
 btn.onclick = function() {
@@ -13,13 +13,7 @@ btn.onclick = function() {
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+btnCancelar.onclick = function() {
   modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}

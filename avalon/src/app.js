00d3b014c81f999850ac0256726e687670a3e93f
paloma <palomavillasboas380@gmail.com>
2023-01-14 16:04:35 -0300

    crud

--- avalon/src/app.js
+++ avalon/src/app.js
@@ -26,6 +26,9 @@ app.use('/users', usersRouter);
 app.use('/produto', produtoRouter);
 
 
+
+
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
--- avalon/src/routes/produto.js
+++ avalon/src/routes/produto.js
@@ -2,13 +2,16 @@ var express = require('express');
 const estiloProdutosController = require('../controllers/estiloProdutosController');
 const detalhesController = require('../controllers/detalhesController');
 const cadastroProdutosController = require('../controllers/cadastroProdutosController');
+const ServicosController = require('../controllers/servicosController')
 var router = express.Router();
 router.get('/cadastroProdutos', cadastroProdutosController.listacadastroProdutos);
 router.get('/detalhes', detalhesController.listadetalhes);
 router.get('/estiloProdutos', estiloProdutosController.listaestiloProdutos);
-
+router.get('/admin', ServicosController.mostraAdminServicos);
 
 router.get('/carrinho', function (req, res, next) {
     res.render('carrinho', { title: 'Express', css: 'carrinho' });
+    router.delete('../remove', ServicosController.deletaServico)
   });
+
 module.exports = router;

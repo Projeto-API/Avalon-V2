62c5b15fe92798edce6d2655685ede12343cc433
IgorCamposRatao <igorcamposratao@gmail.com>
2023-01-14 19:08:55 -0300

    pagina crud refeita do 0

--- avalon/public/javascripts/crud.js
+++ avalon/public/javascripts/crud.js
@@ -1,19 +1,94 @@
-// Get the modal
-var modal = document.getElementById("box-cadastro");
+const modal = document.querySelector('.modal-container')
+const tbody = document.querySelector('tbody')
+const sTitulo = document.querySelector('#titulo')
+const sPreco = document.querySelector('#preco')
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+const btnSalvar = document.querySelector('#btnSalvar')
 
-// Get the <span> element that closes the modal
-var btnCancelar = document.getElementById("cancelarModal");
+let itens
+let id
 
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
+function openModal(edit = false, index = 0) {
+    modal.classList.add('active')
+
+    modal.onclick = e => {
+        if (e.target.className.indexOf('modal-container') !== -1) {
+            modal.classList.remove('active')
+        }
+    }
+
+    if (edit) {
+        sTitulo.value = itens[index].titulo
+        sPreco.value = itens[index].preco
+        id = index
+    } else {
+        sTitulo.value = ''
+        sPreco.value = ''
+    }
+
 }
 
-// When the user clicks on <span> (x), close the modal
-btnCancelar.onclick = function() {
-  modal.style.display = "none";
+function editItem(index) {
+
+    openModal(true, index)
 }
 
+function deleteItem(index) {
+    itens.splice(index, 1)
+    setItensBD()
+    loadItens()
+}
+
+function insertItem(item, index) {
+    let tr = document.createElement('tr')
+
+    tr.innerHTML = `
+    <td><input type="checkbox"></td>
+    <td>${item.index}</td>
+    <td>${item.titulo}</td>
+    <td>R$ ${item.preco}</td>
+    <td class="acao">
+      <button onclick="editItem(${index})"><i class='bx bx-edit' ></i></button>
+    </td>
+    <td class="acao">
+      <button onclick="deleteItem(${index})"><i class='bx bx-trash'></i></button>
+    </td>
+  `
+    tbody.appendChild(tr)
+}
+
+btnSalvar.onclick = e => {
+
+    if (sTitulo.value == '' || sPreco.value == '') {
+        return
+    }
+
+    e.preventDefault();
+
+    if (id !== undefined) {
+        itens[id].titulo = sTitulo.value
+        itens[id].preco = sPreco.value
+    } else {
+        itens.push({ 'titulo': sTitulo.value, 'preco': sPreco.value})
+    }
+
+    setItensBD()
+
+    modal.classList.remove('active')
+    loadItens()
+    id = undefined
+}
+
+function loadItens() {
+    itens = getItensBD()
+    tbody.innerHTML = ''
+    itens.forEach((item, index) => {
+        insertItem(item, index)
+    })
+
+}
+
+const getItensBD = () => JSON.parse(localStorage.getItem('dbfunc')) ?? []
+const setItensBD = () => localStorage.setItem('dbfunc', JSON.stringify(itens))
+
+loadItens()

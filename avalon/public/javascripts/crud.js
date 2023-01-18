ff3de686e185b88ce7e2af76b55dfa94f887f7d3
IgorCamposRatao <igorcamposratao@gmail.com>
2023-01-18 00:07:58 -0300

    mais itens add ao form crud

--- avalon/public/javascripts/crud.js
+++ avalon/public/javascripts/crud.js
@@ -1,9 +1,6 @@
 const modal = document.querySelector('.modal-container')
 const tbody = document.querySelector('tbody')
-const sTitulo = document.querySelector('#titulo')
-const sPreco = document.querySelector('#preco')
 
-const btnSalvar = document.querySelector('#btnSalvar')
 
 let itens
 let id
@@ -16,77 +13,5 @@ function openModal(edit = false, index = 0) {
             modal.classList.remove('active')
         }
     }
-
-    if (edit) {
-        sTitulo.value = itens[index].titulo
-        sPreco.value = itens[index].preco
-        id = index
-    } else {
-        sTitulo.value = ''
-        sPreco.value = ''
-    }
-
+ 
 }
-
-// function editItem(index) {
-
-//     openModal(true, index)
-// }
-
-// function deleteItem(index) {
-//     itens.splice(index, 1)
-//     setItensBD()
-//     loadItens()
-// }
-
-// function insertItem(item, index) {
-//     let tr = document.createElement('tr')
-
-//     tr.innerHTML = `
-//     <td><input type="checkbox"></td>
-//     <td>${item.index}</td>
-//     <td>${item.titulo}</td>
-//     <td>R$ ${item.preco}</td>
-//     <td class="acao">
-//       <button onclick="editItem(${index})"><i class='bx bx-edit' ></i></button>
-//     </td>
-//     <td class="acao">
-//       <button onclick="deleteItem(${index})"><i class='bx bx-trash'></i></button>
-//     </td>
-//   `
-//     tbody.appendChild(tr)
-// }
-
-
-
-// btnSalvar.onclick = e => {
-
-
-
-//     e.preventDefault();
-
-//     if (id !== undefined) {
-//         itens[id].titulo = sTitulo.value
-//         itens[id].preco = sPreco.value
-//     } else {
-//         itens.push({ 'titulo': sTitulo.value, 'preco': sPreco.value})
-//     }
-
-//     setItensBD()
-
-//     modal.classList.remove('active')
-//     loadItens()
-//     id = undefined
-// }
-
-// function loadItens() {
-//     itens = getItensBD()
-//     tbody.innerHTML = ''
-//     itens.forEach((item, index) => {
-//         insertItem(item, index)
-//     })
-
-// }
-
-
-// loadItens()
--- avalon/src/models/produtoModel.js
+++ avalon/src/models/produtoModel.js
@@ -6,19 +6,51 @@ const nomeArquivoProdutos = path.join(__dirname, '../database/produtos.json');
 
 
 module.exports = {
-  index () {
+  index() {
     return produtos
   },
 
-  armazenar (produtos) {
+  armazenar(produtos) {
     fs.writeFileSync(nomeArquivoProdutos, JSON.stringify(produtos));
   },
 
   // criar ({ nome, valor, descricao }) {
 
-  criar ({ nome, valor }) {
-   
-    produtos.push({ id: produtos[produtos.length -1].id + 1, nome, valor });
+  criar({
+    nome,
+    valor,
+    categoria,
+    autor,
+    editora,
+    idioma,
+    paginas,
+    publicacao,
+    dimensoes,
+    acabamento,
+    isbnUm,
+    isbnDois,
+    sinopse,
+    imagemCapa,
+    imagensMiniaturas }) {
+
+    produtos.push({
+      id: produtos[produtos.length - 1].id + 1, 
+      nome,
+      valor,
+      categoria,
+      autor,
+      editora,
+      idioma,
+      paginas,
+      publicacao,
+      dimensoes,
+      acabamento,
+      isbnUm,
+      isbnDois,
+      sinopse,
+      imagemCapa,
+      imagensMiniaturas
+    });
     this.armazenar(produtos);
   }
 
@@ -29,7 +61,7 @@ module.exports = {
   //     id: produtos[produtos.length -1].id + 1,
   //     nome: req.body.nome,
   //     valor: req.body.valor
-     
+
   //   }
 
   //   // enviando para o produtos JSON
@@ -38,33 +70,33 @@ module.exports = {
 
   // },
 
-//   buscar (id) {
-//     return produtos.find(produto => produto.id == id);
-//   },
+  //   buscar (id) {
+  //     return produtos.find(produto => produto.id == id);
+  //   },
 
 
-//   atualizar (id, { nome, valor, descricao }) {
-//     // validacoes para parar a execução
-//     if (!id) return
+  //   atualizar (id, { nome, valor, descricao }) {
+  //     // validacoes para parar a execução
+  //     if (!id) return
 
-//     if (!nome || !valor || !descricao) return
+  //     if (!nome || !valor || !descricao) return
 
-// // criacao de variavel auxiliar
-//     const produto = this.buscar(id);
-// //apos localizar o id, sobrescreve o valor dos campos.
-//     produto.nome = nome;
-//     produto.valor = valor;
-//     produto.descricao = descricao;
+  // // criacao de variavel auxiliar
+  //     const produto = this.buscar(id);
+  // //apos localizar o id, sobrescreve o valor dos campos.
+  //     produto.nome = nome;
+  //     produto.valor = valor;
+  //     produto.descricao = descricao;
 
 
-//     this.armazenar(produtos);
-//   },
+  //     this.armazenar(produtos);
+  //   },
 
-//   deletar (id) {
-//     //se o id não é passado, para a execução
-//     if (!id) return
+  //   deletar (id) {
+  //     //se o id não é passado, para a execução
+  //     if (!id) return
 
-//    produtos = produtos.filter(produto => produto.id != id);
-//    this.armazenar(produtos);
-//   }
+  //    produtos = produtos.filter(produto => produto.id != id);
+  //    this.armazenar(produtos);
+  //   }
 }

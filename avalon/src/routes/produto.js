dee0ba711ee1aaefac30be8170265820dbc3cec4
paloma <palomavillasboas380@gmail.com>
2023-01-30 09:52:31 -0300

    etc

--- avalon/src/routes/produto.js
+++ avalon/src/routes/produto.js
@@ -1,18 +1,18 @@
 var express = require('express');
 var router = express.Router();
-var multer = require('multer')
+// var multer = require('multer')
 var path = require('path')
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-  cb(null, "public/images/produtos")
-  },
-  filename: (req, file, cb) => {
-  cb(null, date.now() + path.extname(file.originalname))
-  }
-})
+// const storage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//   cb(null, "public/images/produtos")
+//   },
+//   filename: (req, file, cb) => {
+//   cb(null, date.now() + path.extname(file.originalname))
+//   }
+// })
 
-var upload = multer({storage: storage})
+// var upload = multer({storage: storage})
 
 const estiloProdutosController = require('../controllers/estiloProdutosController');
 const detalhesController = require('../controllers/detalhesController');
@@ -32,7 +32,7 @@ router.get('/estiloProdutos', estiloProdutosController.listaestiloProdutos);
 
   // C - Criação de novos serviços
 router.get('/admin', produtoController.admin);
-router.post('/admin', upload.single("capaImg"),produtoController.criar);
+// router.post('/admin', upload.single("capaImg"),produtoController.criar);
 
 
 

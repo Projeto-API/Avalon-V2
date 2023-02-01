573a0aeeb0a898e3f5bdd6cf8c567ea3b5475bec
paloma <palomavillasboas380@gmail.com>
2023-02-01 14:53:27 -0300

    carrinho

--- avalon/public/javascripts/carrinhoFlutuante.js
+++ avalon/public/javascripts/carrinhoFlutuante.js
@@ -18,52 +18,26 @@ function excluirDentroCarrinho(excluiProduto) {
     }
     
 }
+function incluirDentroCarrinho(incluiProduto) {
 
-// function incluirDentroCarrinho() {
-//     let em = document.createElement('em')
-// //     em.innerHTML = `<div class="produto" id="produto4">
-// //     <img class="imagemProduto" src="../produtos/my broken mariko/mariko-capa.jpg" alt="">
-// //     <div class="informationProduto">        
-// //         <div class="nomeProduto">
-// //             <h5 id="produtoTitulo">MY BROKEN MARIKO</h5>
-// //         </div>
-// //         <div class="informationProdutoValue">
-// //             <div>
-// //                 <a href="" class="diminuirQtd">-</a>
-// //                 <input type="number" class="quantidadeValue" value="1">
-// //                 <a href="" class="aumentarQtd">+</a>
-// //             </div>
-// //             <span id="preco"> R$ 39,90 </span>
-// //             <button id="excluir"  onclick="excluirDentroCarrinho('produto4')">X</button>
-// //         </div>
-// //     </div>
-// // </div>`
-//     document.querySelector('#containerProduto').appendChild(em)
-// }
-var arrayDiv = [];
+  let arrayDiv = document.getElementsByTagName("div");
 
-function incluirDentroCarrinho(produto) {
-    arrayDiv.push(produto);
-  console.log(produto + " foi adicionado ao carrinho.");
-}
-
-function removeFromCart(produto) {
-  var index = cart.indexOf(produto);
-  if (index > -1) {
-    cart.splice(index, 1);
-    console.log(produto + " foi removido do carrinho.");
-  } else {
-    console.log(produto + " não foi encontrado no carrinho.");
+  for (let index = 0; index < arrayDiv.length; index++) {
+      var element = arrayDiv[index]
+      if(element.id == incluiProduto){
+          element.style.display= "none"
+      }
   }
+  
 }
-
-function viewCart() {
-  if (cart.length > 0) {
+function verDentroCarrinho() {
+  if (arrayDiv.length > 0) {
     console.log("Itens no carrinho:");
-    for (var i = 0; i < cart.length; i++) {
-      console.log(cart[i]);
+    for (let i = 0; i < arrayDiv.length; i++) {
+      console.log(arrayDiv[i]);
     }
   } else {
     console.log("O carrinho está vazio.");
   }
 }
+

6493b89e49ab86aae86f52832c03585bec313df8
IgorCamposRatao <igorcamposratao@gmail.com>
2023-02-21 14:03:13 -0300

    menu hamburguer finalizado

--- avalon/public/javascripts/menuHamburguer.js
+++ avalon/public/javascripts/menuHamburguer.js
@@ -3,20 +3,10 @@
 
     function abrirHamburguer() {
         
-        let hamburger = document.getElementById("chaveHamburguer")
-        if(hamburger.style.left =="-350px"){
-            hamburger.style.left = "0px"
-        } else {
-            hamburger.style.left = "-350px";
-        }
+        document.querySelector('.sidebar').style.left = "0"
+
     }
-    
-    
-    // document.getElementById("chaveHamburguer")
-    // let navigation = document.querySelector("navigation")
-    
-    // hamburger.onclick = () => {
-    //     navigation.style.transform = "translateX(100)"
-    // }
-    
-// }
+
+    function fecharMenu() {
+        document.querySelector('.sidebar').style.left = "-20rem"
+    }

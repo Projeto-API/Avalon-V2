53d3ed99205b12f30b9b1e1d155e82d6e75a9902
paloma <palomavillasboas380@gmail.com>
2023-02-23 12:50:59 -0300

    erros

--- avalon/routes/home.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-const homeController = require('../controllers/homeController');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', homeController.listahome);
- 
-
-module.exports = router;
--- avalon/routes/users.js
+++ avalon/routes/users.js
@@ -1,3 +1,4 @@
+
 var express = require('express');
 var router = express.Router();
 
@@ -6,4 +7,5 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+module.exports = router;
 module.exports = router;

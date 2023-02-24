001ffc2433e0b10788617a4d37153fb80b866622
IgorCamposRatao <igorcamposratao@gmail.com>
2023-02-23 21:11:20 -0300

    correção pasta models

--- avalon/public/javascripts/script.js
+++ avalon/public/javascripts/script.js
@@ -27,8 +27,7 @@ const carousel = document.getElementById("carrousel"),
   next = document.getElementById("next"),
   prev = document.getElementById("prev");
 
-next.forEach(button => {
-  button.addEventListener("click", e => {
+next.addEventListener("click", e => {
     carousel.scrollBy(width + gap, 0);
     if (carousel.scrollWidth !== 0) {
       prev.style.display = "flex";
@@ -36,8 +35,7 @@ next.forEach(button => {
     if (content.scrollWidth - width - gap <= carousel.scrollLeft + width) {
       next.style.display = "none";
     }
-  })
-});
+  });
 prev.addEventListener("click", e => {
   carousel.scrollBy(-(width + gap), 0);
   if (carousel.scrollLeft - width - gap <= 0) {
@@ -52,31 +50,3 @@ let width = carousel.offsetWidth;
 window.addEventListener("resize", e => (width = carousel.offsetWidth));
 
 
-// const gap = .1;
-
-// const carousel = document.getElementById("carrousel"),
-//   content = document.getElementById("gallery"),
-//   next = document.getElementById("next"),
-//   prev = document.getElementById("prev");
-
-// next.addEventListener("click", e => {
-//   carousel.scrollBy(width + gap, 0);
-//   if (carousel.scrollWidth !== 0) {
-//     prev.style.display = "flex";
-//   }
-//   if (content.scrollWidth - width - gap <= carousel.scrollLeft + width) {
-//     next.style.display = "none";
-//   }
-// });
-// prev.addEventListener("click", e => {
-//   carousel.scrollBy(-(width + gap), 0);
-//   if (carousel.scrollLeft - width - gap <= 0) {
-//     prev.style.display = "none";
-//   }
-//   if (!content.scrollWidth - width - gap <= carousel.scrollLeft + width) {
-//     next.style.display = "flex";
-//   }
-// });
-
-// let width = carousel.offsetWidth;
-// window.addEventListener("resize", e => (width = carousel.offsetWidth));

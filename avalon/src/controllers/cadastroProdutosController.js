6514cf304479113e29e1fcc0075f4e89c24cbfe9
amandakrine <amandakrine@hotmail.com>
2023-03-14 19:21:34 -0400

    validacoes cadastro produto

--- avalon/src/controllers/cadastroProdutosController.js
+++ avalon/src/controllers/cadastroProdutosController.js
@@ -1,9 +1,22 @@
-const cadastroProdutosModel = require('../models/cadastroProdutosModel')
+const cadastroProdutosModel = require('../models/cadastroProdutosModel');
+const { validationResult } = require('express-validator');
+const e = require('express');
 
 module.exports = {
     listacadastroProdutos: (req, res) => {
         const cadastroProdutos = cadastroProdutosModel.index();
        
         res.render('cadastroProdutos', { title: 'Express', css: 'cadastroProdutos', cadastroProdutos });
+    },
+
+    //Validações Cadastro Produtos:
+    register: (req, res) => {
+        let errors = validationResult(req);
+
+        if (errors.isEmpty()){
+
+        } else {
+            res.render('register', {errors: errors.mapped(), old: req.body});
+        }
     }
 }
--- avalon/src/routes/produto.js
+++ avalon/src/routes/produto.js
@@ -3,8 +3,30 @@ const estiloProdutosController = require('../controllers/estiloProdutosControlle
 const detalhesController = require('../controllers/detalhesController');
 const cadastroProdutosController = require('../controllers/cadastroProdutosController');
 const ServicosController = require('../controllers/servicosController')
+const { body } = require('express-validator')
+
+
+//Validações Cadastro Produtos:
+const validacoes = [
+  body('tituloLivro').notEmpty().withMessage('É obrigatório preencher o nome').bail().isString()
+  .withMessage('É obrigatório o uso de caracter alfabético'),
+  body('autor').notEmpty().withMessage('É obrigatório preencher o autor').bail().isString(),
+  body('editora').notEmpty().withMessage('É obrigatório preencher a editora').bail().isString(),
+  body('idioma').notEmpty().withMessage('É obrigatório preencher o idioma').bail().isString(),
+  body('paginas').notEmpty().withMessage('É obrigatório preencher o número de páginas').bail().isNumeric(),
+  body('publicacao').notEmpty().withMessage('É obrigatório preenhcer a data da publicação').bail().isDate(),
+  body('dimensoes').notEmpty().withMessage('É obrigatório preencher as dimensões').bail().isString(),
+  body('acabamento').notEmpty().withMessage('É obrigatório preencher o acabamento').bail().isString(),
+  body('isbn').notEmpty().withMessage('É obrigatório preecher o ISBN-10').bail().isNumeric(),
+  body('preco').notEmpty().withMessage('É obrigatório preencher o preço').bail().isNumeric(),
+  body('sinopse').notEmpty().withMessage('É obrigatório preencher a sinopse').bail().isString,
+];
+
+
+
+
 var router = express.Router();
-router.get('/cadastroProdutos', cadastroProdutosController.listacadastroProdutos);
+router.get('/cadastroProdutos', validacoes, cadastroProdutosController.listacadastroProdutos);
 router.get('/detalhes', detalhesController.listadetalhes);
 router.get('/estiloProdutos', estiloProdutosController.listaEstiloProdutos);
 

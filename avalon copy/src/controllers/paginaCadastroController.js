8c76b87fb5433b499e88cf604bb99e1f15fe4b4d
Joao <pouljp@bol.com.br>
2023-03-18 20:29:02 -0300

    Excluindo arquivos temporarios

--- avalon copy/src/controllers/paginaCadastroController.js
+++
@@ -1,9 +0,0 @@
-const paginaCadastroModel = require('../models/paginaCadastroModel')
-
-module.exports = {
-    listapaginaCadastro: (req, res) => {
-        const paginaCadastro = paginaCadastroModel.users();
-       
-        res.render('paginaCadastro', { title: 'Express', css: 'paginaCadastro', paginaCadastro });
-    }
-}
--- carrinho/routes/users.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-const UserController = require('../controllers/UserController')
-
-/* GET users listing. */
-router.get('/', UserController.index);
-
-module.exports = router;

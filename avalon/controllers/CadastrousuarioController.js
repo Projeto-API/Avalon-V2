05479e2990be673a28fb41e0debe61b591d41ab5
paloma <palomavillasboas380@gmail.com>
2023-03-20 08:30:26 -0300

    controlles

--- avalon/controllers/CadastrousuarioController.js
+++ avalon/controllers/CadastrousuarioController.js
@@ -1,36 +1,9 @@
-const fs = require('fs');
-const path = require('path');
-const { Op } = require('sequelize');
-const { Usuario } = require('../models');
-
-Op
-
-
+const CadastroUsuarioModel = require('../models/CadastroUsuarioModel')
 
 module.exports = {
-  index: async (req, res) => {
-    try { 
-      res.render('cadastroUsuario')
-
-    } catch (erro) {
-      console.log(erro)
-      let alert = require('alert');
-      alert("ERRO 500 - Erro interno do servidor!")
+    listaCadastroUsuario: (req, res) => {
+        // const CadastroUsuario = CadastroUsuarioModel.users();
+       
+        res.render('CadastroUsuario', { title: 'Express', css: 'CadastroUsuario' });
     }
-  },
-
-
-  criar: async (req, res) => {
-    try {
-      const { email, password, nome, sobrenome, data_nascimento, doc_identificacao, cep, endereco, foto } = req.body
-
-      await Usuario.create({ email, password, nome, sobrenome, data_nascimento, doc_identificacao, cep, endereco, foto })
-
-      res.redirect('/users/login');
-
-    } catch (erro) {
-      let alert = require('alert');
-      alert("ERRO 500 - Erro interno do servidor!")
-    }
-  }
 }
--- avalon/controllers/homeController.js
+++ avalon/controllers/homeController.js
@@ -1,24 +1,9 @@
-const { Livro, Editora, Autor, Categoria } = require('../models');
+const homeModel = require('../models/homeModel')
 
 module.exports = {
-  index: async (req, res) => {
-    try {
-      const livros = await Livro.findAll();
-      const livrosLancamento = await Livro.findAll({
-        limit: 8, 
-        order: [['id','DESC']]
-      });
-      const editoras = await Editora.findAll();
-      const autores = await Autor.findAll();
-      const categorias = await Categoria.findAll();
-
-
-      res.render('home', { livros, livrosLancamento, editoras, categorias, autores })
-
-    } catch (erro) {
-      let alert = require('alert');
-      alert("ERRO 500 - Erro interno do servidor!")
+    listahome: (req, res) => {
+        const home = homeModel.index();
+       
+        res.render('home', { title: 'Express', css: 'home',home });
     }
-  },
-
 }

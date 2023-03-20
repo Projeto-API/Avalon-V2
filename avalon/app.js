215e3b847b45f62402f0712d88cd5cb024c5e59e
paloma <palomavillasboas380@gmail.com>
2023-03-20 08:14:08 -0300

    error

--- avalon/app.js
+++ avalon/app.js
@@ -7,12 +7,7 @@ const methodOverride = require('method-override');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
-<<<<<<< HEAD
 const bibliotecaRouter = require('./routes/biblioteca');
-=======
-const adminRouter = require('./routes/admin');
-
->>>>>>> igorcr
 
 const app = express();
 
@@ -29,12 +24,7 @@ app.use(methodOverride('_method'));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-<<<<<<< HEAD
 app.use('/biblioteca', bibliotecaRouter);
-=======
-app.use('/admin', adminRouter);
-
->>>>>>> igorcr
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -52,8 +42,4 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-<<<<<<< HEAD
 module.exports = app;
-=======
-module.exports = app;
->>>>>>> igorcr

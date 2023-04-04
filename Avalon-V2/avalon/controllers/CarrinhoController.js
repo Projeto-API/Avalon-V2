cf3bb68188750e2146936bf7a6d57efc6d75bb9c
Joao <pouljp@bol.com.br>
2023-04-04 01:14:19 -0300

    enviando para banco de dados

--- Avalon-V2/avalon/controllers/CarrinhoController.js
+++ Avalon-V2/avalon/controllers/CarrinhoController.js
@@ -1,14 +1,18 @@
-const { Carrinho, TodososlivrosModel } = require('../models');
+const { Carrinho, Livro , TodososlivrosModel } = require('../models');
 const CarrinhoController = {
   carrinho: async (req, res) => {
     let valorFrete = null;
-    let livros = await Carrinho.findAll();
-    let total = 0;
-    livros.forEach(livro => {
-      let itemTotal = livro.quantidade * livro.preco;
-      total += itemTotal;
+    Carrinho.findAll().then(dentroCarrinho => {
+      let total = 0;
+      dentroCarrinho.forEach(unid => {
+        let itemTotal = unid.quantidade * unid.livros_id.preco;
+        total += itemTotal;
     });
-    return res.render('carrinho', { livros, valorFrete, total });
+    return res.render('carrinho', { dentroCarrinho, valorFrete, total });
+
+  }).catch(error => {
+    console.log(error);
+  });
   },
   add: async (req, res) => {
     const livros = JSON.parse(req.body.produtos)
@@ -26,23 +30,34 @@ const CarrinhoController = {
     res.redirect('/carrinho');
   },
   teste: async (req, res) => {
+    var convert = JSON.parse(req.body.produtosCarrinho);
     try {
-      const { produtosJson } = req.body;
-      const listaLivros = JSON.parse(produtosJson);
-      console.log("lista de produto",listaLivros )
+      console.log('Array: ' + JSON.stringify(convert))
+      // console.log("entrei no Try")
+
+      convert.forEach(async element =>{
+        const objLivroCarrinho = await Livro.findOne({ where: { titulo: element.productTitle } });
+        console.log('vampeta : '+ objLivroCarrinho.titulo)
+        console.log('productQuantity: ' + element.productQuantity)
+        const carrinhoSalvo = await Carrinho.create({ quantidade: element.productQuantity , livros_id: objLivroCarrinho.id ,usuarios_id: 1 })
+          
+      });
+        // const listaLivros = JSON.stringify(produtosJson);
+      // console.log("lista de produto" + listaLivros );
       // Add each book in listaLivros to the shopping cart
-      for (const livro of listaLivros) {
-        await Carrinho.create({
-          titulo: livro.productTitle,
-          autor: livro.productAuthor,
-          preco: livro.productPrice,
-          quantidade: 1 // assuming the default quantity is 1
-        });
-        res.redirect('/carrinho');
-      }
+      // for (const livro of listaLivros) {
+      //   await Carrinho.create({
+      //     titulo: livro.productTitle,
+      //     autor: livro.productAuthor,
+      //     preco: livro.productPrice,
+      //     quantidade: 1 // assuming the default quantity is 1
+      //   });
+         res.redirect('/carrinho');
+
     } catch (erro) {
       let alert = require('alert');
-      alert("ERRO 500 - Erro interno do servidor!")
+      console.log('Ferrou !');
+      alert("deu M....")
     }
   }
 };

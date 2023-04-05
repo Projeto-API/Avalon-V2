26605216c4eb69d295341d55c2ee3d6a27af90ca
Joao <pouljp@bol.com.br>
2023-04-04 23:27:15 -0300

    alterações lais

--- Avalon-V2/avalon/controllers/CarrinhoController.js
+++ Avalon-V2/avalon/controllers/CarrinhoController.js
@@ -1,18 +1,38 @@
+const { where } = require('sequelize');
 const { Carrinho, Livro , TodososlivrosModel } = require('../models');
 const CarrinhoController = {
   carrinho: async (req, res) => {
-    let valorFrete = null;
-    Carrinho.findAll().then(dentroCarrinho => {
-      let total = 0;
-      dentroCarrinho.forEach(unid => {
-        let itemTotal = unid.quantidade * unid.livros_id.preco;
-        total += itemTotal;
-    });
-    return res.render('carrinho', { dentroCarrinho, valorFrete, total });
+    try{
+      const carrinhoFinal = []
+    let pegaDados = await Carrinho.findAll({where: {usuarios_id: 1 }})
+    pegaDados.forEach(carrinho =>
+      
+      {
+      const livro = Livro.findOne({where :{id: carrinho.livros_id} })
+      console.log("estou aqui livro" + livro)
+      
+      carrinhoFinal.push(carrinho,livro.imagens, livro.titulo , livro.preco )
+       
+    })
+    console.log("estou aqui carrinhoFinal salvo" + carrinhoFinal)
 
-  }).catch(error => {
-    console.log(error);
-  });
+    res.render('carrinho', {carrinho : carrinhoFinal });
+
+    
+    // let valorFrete = null;
+    // Carrinho.findAll().then(dentroCarrinho => {
+    //   let total = 0;
+    //   dentroCarrinho.forEach(unid => {
+    //     let itemTotal = unid.quantidade * unid.livros_id.preco;
+    //     total += itemTotal;
+    // });
+    // return res.render('carrinho', { dentroCarrinho, valorFrete, total });
+
+  } catch (erro) {
+    let alert = require('alert');
+    console.log('Ferrou !');
+    alert("deu M....")
+  }
   },
   add: async (req, res) => {
     const livros = JSON.parse(req.body.produtos)
@@ -31,17 +51,23 @@ const CarrinhoController = {
   },
   teste: async (req, res) => {
     var convert = JSON.parse(req.body.produtosCarrinho);
+    
     try {
       console.log('Array: ' + JSON.stringify(convert))
       // console.log("entrei no Try")
 
       convert.forEach(async element =>{
         const objLivroCarrinho = await Livro.findOne({ where: { titulo: element.productTitle } });
-        console.log('vampeta : '+ objLivroCarrinho.titulo)
-        console.log('productQuantity: ' + element.productQuantity)
+        // console.log('Titulo Crrinho : '+ objLivroCarrinho.titulo)
+        // console.log('Quantidade: ' + element.productQuantity)
         const carrinhoSalvo = await Carrinho.create({ quantidade: element.productQuantity , livros_id: objLivroCarrinho.id ,usuarios_id: 1 })
+
+        
           
       });
+
+
+
         // const listaLivros = JSON.stringify(produtosJson);
       // console.log("lista de produto" + listaLivros );
       // Add each book in listaLivros to the shopping cart
--- Avalon-V2/avalon/public/javascripts/carrinhoscript.js
+++ Avalon-V2/avalon/public/javascripts/carrinhoscript.js
@@ -2,78 +2,78 @@
 
 
 
-// //----------PAGINA DE ITENS NO CARRINHO ------////
-// if (document.readyState == "loading") {
-//   document.addEventListener("DOMContentLoaded", ready);
-// } else {
-//   ready();
-// }
-// function ready() {
-//   const removeLivro = document.getElementsByClassName("remove-livro-button");
-//   for (let i = 0; i < removeLivro.length; i++) {
-//     removeLivro[i].addEventListener("click", removeProduct);
-//   }
-//   const quantityInputs = document.getElementsByClassName("produto-quantidade");
-//   for (let i = 0; i < quantityInputs.length; i++) {
-//     quantityInputs[i].addEventListener("change", checkIfInputIsNotNull);
-//   }
-//   const addToCartButtons = document.getElementsByClassName("comprar");
-//   for (let i = 0; i < addToCartButtons.length; i++) {
-//     addToCartButtons[i].addEventListener("click", addProductToCart);
-//   }
-//   carrinhoAtualizacao(true);
-// }
-// function checkIfInputIsNotNull(event) {
-//   if (event.target.value === "0") {
-//     event.target.parentElement.parentElement.remove();
-//   }
-//   carrinhoAtualizacao(false);
-// }
-// function removeProduct(event) {
-//   const buttonClicked = event.target;
-//   buttonClicked.parentElement.parentElement.remove();
-//   carrinhoAtualizacao(false);
-// }
-// function addProductToCart(event) {
-//   carrinhoAtualizacao(false);
-// }
-// function carrinhoAtualizacao(primeiroCarregamento) {
-//   let somaCarrinho = 0;
-//   const desconto = 9;
-//   const cifrao = "R$ ";
-//   let carLivros = document.getElementById("produto-carrinho").children;
+//----------PAGINA DE ITENS NO CARRINHO ------////
+if (document.readyState == "loading") {
+  document.addEventListener("DOMContentLoaded", ready);
+} else {
+  ready();
+}
+function ready() {
+  const removeLivro = document.getElementsByClassName("remove-livro-button");
+  for (let i = 0; i < removeLivro.length; i++) {
+    removeLivro[i].addEventListener("click", removeProduct);
+  }
+  const quantityInputs = document.getElementsByClassName("produto-quantidade");
+  for (let i = 0; i < quantityInputs.length; i++) {
+    quantityInputs[i].addEventListener("change", checkIfInputIsNotNull);
+  }
+  const addToCartButtons = document.getElementsByClassName("comprar");
+  for (let i = 0; i < addToCartButtons.length; i++) {
+    addToCartButtons[i].addEventListener("click", addProductToCart);
+  }
+  carrinhoAtualizacao(true);
+}
+function checkIfInputIsNotNull(event) {
+  if (event.target.value === "0") {
+    event.target.parentElement.parentElement.remove();
+  }
+  carrinhoAtualizacao(false);
+}
+function removeProduct(event) {
+  const buttonClicked = event.target;
+  buttonClicked.parentElement.parentElement.remove();
+  carrinhoAtualizacao(false);
+}
+function addProductToCart(event) {
+  carrinhoAtualizacao(false);
+}
+function carrinhoAtualizacao(primeiroCarregamento) {
+  let somaCarrinho = 0;
+  const desconto = 9;
+  const cifrao = "R$ ";
+  let carLivros = document.getElementById("produto-carrinho").children;
 
-//   for (let i = 0; i < carLivros.length; i++) {
-//     let totalAmount = 0;
-//     let productPrice;
-//     if (primeiroCarregamento) {
-//       carLivros[i].getElementsByClassName("livro-preco")[0].innerText = cifrao + carLivros[i].getElementsByClassName("livro-preco")[0].innerText.replace(".", ",");
-//     }
-//     productPrice = parseFloat(carLivros[i].getElementsByClassName("livro-preco")[0].innerText.replace(".", ",").replace("R$", ""));
-//     productQuantity = carLivros[i].getElementsByClassName("produto-quantidade")[0].value;
-//     totalAmount += productPrice * productQuantity;
-//     somaCarrinho += totalAmount;
-//     totalAmount = totalAmount.toFixed(2);
-//     totalAmount = totalAmount.replace(".", ",");
-//     carLivros[i].getElementsByClassName("cart-total")[0].innerText = cifrao + totalAmount;
-//   }
-//   const descricaoValorZero = cifrao + "0,00";
-//   let totalFinal = somaCarrinho - desconto;
-//   let ajusteDesconto = totalFinal;
-//   somaCarrinho = somaCarrinho.toFixed(2);
-//   somaCarrinho = somaCarrinho.replace(".", ",");
-//   document.getElementById("cart-total-subtotal").innerText = cifrao + somaCarrinho;
-//   document.getElementById("cart-total-subtotal-2").innerText = cifrao + somaCarrinho;
-//   const descontoFormatted = desconto.toFixed(2).replace(".", ",");
-//   const cardTotalDesconto = document.getElementById("cart-total-desconto");
-//   if (somaCarrinho == 0) {
-//     document.getElementById("valor-total").innerText = descricaoValorZero;
-//     cardTotalDesconto.innerText = descricaoValorZero;
-//   } else if (ajusteDesconto < 0) {
-//   } else {
-//     totalFinal = totalFinal.toFixed(2);
-//     totalFinal = totalFinal.replace(".", ",");
-//     document.getElementById("valor-total").innerText = cifrao + totalFinal;
-//     cardTotalDesconto.innerText = "-" + cifrao + descontoFormatted;
-//     }
-//     }
+  for (let i = 0; i < carLivros.length; i++) {
+    let totalAmount = 0;
+    let productPrice;
+    if (primeiroCarregamento) {
+      carLivros[i].getElementsByClassName("livro-preco")[0].innerText = cifrao + carLivros[i].getElementsByClassName("livro-preco")[0].innerText.replace(".", ",");
+    }
+    productPrice = parseFloat(carLivros[i].getElementsByClassName("livro-preco")[0].innerText.replace(".", ",").replace("R$", ""));
+    productQuantity = carLivros[i].getElementsByClassName("produto-quantidade")[0].value;
+    totalAmount += productPrice * productQuantity;
+    somaCarrinho += totalAmount;
+    totalAmount = totalAmount.toFixed(2);
+    totalAmount = totalAmount.replace(".", ",");
+    carLivros[i].getElementsByClassName("cart-total")[0].innerText = cifrao + totalAmount;
+  }
+  const descricaoValorZero = cifrao + "0,00";
+  let totalFinal = somaCarrinho - desconto;
+  let ajusteDesconto = totalFinal;
+  somaCarrinho = somaCarrinho.toFixed(2);
+  somaCarrinho = somaCarrinho.replace(".", ",");
+  document.getElementById("cart-total-subtotal").innerText = cifrao + somaCarrinho;
+  document.getElementById("cart-total-subtotal-2").innerText = cifrao + somaCarrinho;
+  const descontoFormatted = desconto.toFixed(2).replace(".", ",");
+  const cardTotalDesconto = document.getElementById("cart-total-desconto");
+  if (somaCarrinho == 0) {
+    document.getElementById("valor-total").innerText = descricaoValorZero;
+    cardTotalDesconto.innerText = descricaoValorZero;
+  } else if (ajusteDesconto < 0) {
+  } else {
+    totalFinal = totalFinal.toFixed(2);
+    totalFinal = totalFinal.replace(".", ",");
+    document.getElementById("valor-total").innerText = cifrao + totalFinal;
+    cardTotalDesconto.innerText = "-" + cifrao + descontoFormatted;
+    }
+    }

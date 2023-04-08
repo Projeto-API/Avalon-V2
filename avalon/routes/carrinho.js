c10227b9a31a8f86896823bed094c346144c2af5
paloma <palomavillasboas380@gmail.com>
2023-04-08 10:08:39 -0300

    carrinho funcionando

--- avalon/routes/carrinho.js
+++ avalon/routes/carrinho.js
@@ -1,10 +1,16 @@
 var express = require('express');
 var router = express.Router();
 const CarrinhoController = require('../controllers/CarrinhoController')
+const validaCarrinho = require('../middewres/validaCarrinho')
 
 /* GET users listing. */
 router.get('/', CarrinhoController.carrinho);
-router.post('/', CarrinhoController.add);
+// router.post('/', CarrinhoController.add);
 router.delete('/', CarrinhoController.remove);
+router.post('/', CarrinhoController.teste);
+
+// router.post('/', validaCarrinho.validaCarrinho, CarrinhoController.teste);
+
+
 
 module.exports = router;

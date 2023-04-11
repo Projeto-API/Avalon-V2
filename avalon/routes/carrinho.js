efd7d1b63d39ccba52535f3128015dacd9be1b68
IgorCamposRatao <igorcamposratao@gmail.com>
2023-04-11 12:04:21 -0300

    view 'compra efetuada' finalizada

--- avalon/routes/carrinho.js
+++ avalon/routes/carrinho.js
@@ -4,6 +4,7 @@ var router = express.Router();
 const EntregaController = require('../controllers/EntregaController');
 const CheckoutController = require('../controllers/CheckoutController');
 const CarrinhoController = require('../controllers/CarrinhoController')
+const CompraEfetuadaController = require('../controllers/CompraEfetuadaController')
 const auth = require('../middlewares/auth')
 
 /* GET users listing. */
@@ -15,6 +16,7 @@ router.post('/', CarrinhoController.teste);
 
 router.get('/checkout/', auth , CheckoutController.index);
 router.get('/checkout/:id',  CheckoutController.index);
+router.get('/compra-finalizada/', auth, CompraEfetuadaController.index);
 
 
 module.exports = router;
